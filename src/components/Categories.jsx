e39b58e88d25269b6adee23b5909954adbcbd876
JOY19ANYANGO <joyanyango402@gmail.com>
2024-07-21 17:55:35 +0300

    changed landing page design

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,65 +1,40 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faHamburger,
-  faCoffee,
-  faShoppingBag,
-  faShoppingBasket,
-  faPalette,
-  faEye,
-  faBed,
-  faCocktail,
-  faGamepad,
-  faSmile,
-  faSchool,
-  faGraduationCap,
-  faFutbol,
-  faDumbbell,
-  faHospital,
-  faClinicMedical,
-  faMask,
-  faTheaterMasks,
-  faUniversity,
-  faLandmark,
-  faMapMarkedAlt,
-  faGlassCheers,
-} from '@fortawesome/free-solid-svg-icons';
 
 const CategoryIcons = () => {
   return (
-    <div className="absolute bottom-0 w-full flex justify-center mb-8">
-      <div className="grid grid-cols-6 gap-2">
-        {categories.map((category, index) => (
-          <div key={index} className="bg-white text-red-500 p-4 shadow-md h-40 w-40 flex flex-col justify-center items-center">
-            <div className="mb-4">
-              <FontAwesomeIcon icon={category.icon1} className="text-5xl" />
-              <FontAwesomeIcon icon={category.icon2} className="text-5xl ml-4" />
+    <div style={{ marginTop: '100vh' }}  >
+      <h1 className='text-white text-center'> V I E W   C A T E G O R I E S</h1>
+      <div className="w-full flex justify-center bg-black">
+        <div className="grid grid-cols-6 gap-4">
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              className="relative bg-white text-white shadow-md h-60 w-60 flex flex-col justify-center items-center"
+              style={{
+                backgroundImage: `url(${category.icon1})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="absolute top-1/4 left-1/4 h-1/2 w-1/2 bg-black bg-opacity-50 flex items-center justify-center">
+                <div className="text-center">{category.label}</div>
+              </div>
             </div>
-            <div className="text-center">{category.label}</div>
-            <div className="text-center text-xs mt-2">10 LISTINGS</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 const categories = [
-  { icon1: faHamburger, icon2: faCoffee, label: '' },
-  { icon1: faShoppingBag, icon2: faShoppingBasket, label: 'Malls' },
-  { icon1: faPalette, icon2: faEye, label: 'Beauty' },
-  { icon1: faBed, icon2: faCocktail, label: 'Hotels' },
-  { icon1: faGamepad, icon2: faSmile, label: 'Fun & Games' },
-  { icon1: faSchool, icon2: faGraduationCap, label: 'Schools' },
-  { icon1: faFutbol, icon2: faDumbbell, label: 'Sports' },
-  { icon1: faHospital, icon2: faClinicMedical, label: 'Health' },
-  { icon1: faTheaterMasks, icon2: faMask, label: 'Theater' },
-  { icon1: faUniversity, icon2: faLandmark, label: 'Museum' },
-  { icon1: faMapMarkedAlt, icon2: faGlassCheers, label: 'Places' },
-  { icon1: faGlassCheers, icon2: faCocktail, label: 'Bar & Club' },
+  { icon1: "https://media-cdn.tripadvisor.com/media/photo-s/22/17/05/8c/mama-harvest-welcomes.jpg", label: 'RESTAURANTS' },
+  { icon1: "https://cdn.standardmedia.co.ke/sdemedia/sdeimages/wednesday/thumb_twr1p94gzqfq5a4cd3dc98fa1.jpg", label: 'MALLS' },
+  { icon1: "https://www.travelstart.co.ke/blog/wp-content/uploads/2014/06/Brew-Bistro-780x519.jpg", label: 'BARS & CLUBS' },
+  { icon1: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/c1/de/d2/caption.jpg?w=500&h=400&s=1", label: 'GAMES & FUNS' },
+  { icon1: "https://www.pelago.co/img/products/KE-Kenya/3-hours-nairobi-national-museum/f9cc2c9b-a326-4404-8edc-de54c72faf7d_3-hours-nairobi-national-museum-medium.jpg", label: 'MUESUEMS' },
+  { icon1: "https://perfectwildernesstours.com/wp-content/uploads/2023/04/Karura-Forest-Reserve.jpg", label: 'NATURE' },
 ];
 
 export default CategoryIcons;
-
-
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,36 +4,27 @@ import SearchBar from '../components/Searchbar';
 import Header from '../components/Header';
 import AddListings from '../components/AddListings';
 import CategoryIcons from '../components/Categories';
-import PopularAreas from '../components/MostPopular';
 
 const LandingPage = () => {
   return (
-    <div>
-    <div className="font-georgia text-white relative h-screen bg-slate-100 overflow-auto">
-      <div className="absolute top-0 left-0 w-full h-3/4 bg-cover bg-center" style={{ backgroundImage: "url('https://www.travelsafe-abroad.com/wp-content/uploads/mustafa-omar-Zkao_QBEjk8-unsplash.jpg')" }}>
-        <div className="fixed top-0 left-0 w-full z-10">
+    <div className=' bg-black text-white'>
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center" style={{ backgroundImage: "url('https://www.travelsafe-abroad.com/wp-content/uploads/mustafa-omar-Zkao_QBEjk8-unsplash.jpg')" }}>
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div> {/* Semi-transparent overlay */}
+        
+        <div className="relative z-10">
           <NavBar />
           <AddListings />
           <Header />
         </div>
-        <div className="pt-32 mt-12">
+        <div className="relative pt-32 mt-12 z-10">
           <SearchBar />
         </div>
       </div>
-    
-      <div className="absolute bottom-0 left-0 w-full h-1/4 flex justify-center items-center -mb-12">
-        <CategoryIcons />
-      </div>
       
+      <CategoryIcons />
     </div>
-    <div className='bg-slate-100'>
-      <PopularAreas/>
-    </div>
-  </div>
   );
 };
 
 export default LandingPage;
-
-
 
